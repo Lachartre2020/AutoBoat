c2eb6553e252aed73c8e4f0d2eac59cfe64a1de8
Lachartre2020 <s.lachartre@hotmail.fr>
2021-04-14 00:24:06 +0200

    Etude de class

---
+++ src/Hangar.java
@@ -0,0 +1,12 @@
+
+public class Hangar {
+
+	public static void main(String[] args) {
+		Car car = new Car("Clio", 100);
+		Boat boat = new Boat("Titanic", 150);
+		
+		System.out.println(car.doStuff());
+		System.out.println(boat.doStuff());
+	}
+
+}
